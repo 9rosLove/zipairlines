611b702806eac8db90b38fd735e71059ab311891
9rosLove <yarik2300@gmail.com>
2024-01-15 19:29:12 +0200

    add permission_classes in AirplaneViewset

--- airplanes/views.py
+++ airplanes/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 from airplanes.models import Airplane
 from airplanes.serializers import AirplaneSerializer, AirplaneDetailSerializer
@@ -7,6 +8,7 @@ from airplanes.serializers import AirplaneSerializer, AirplaneDetailSerializer
 class AirplaneViewset(viewsets.ModelViewSet):
     queryset = Airplane.objects.all()
     serializer_class = AirplaneSerializer
+    permission_classes = [IsAuthenticated, ]
 
     def get_serializer_class(self):
         serializer_class = self.serializer_class
