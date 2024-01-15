fbbd8363e4676e5b473636626c55e82672889471
9rosLove <yarik2300@gmail.com>
2024-01-15 16:25:54 +0200

    add Airpline serializers, views and urld

--- airplanes/views.py
+++ airplanes/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from airplanes.models import Airplane
+from airplanes.serializers import AirplaneSerializer, AirplaneDetailSerializer
+
+
+class AirplaneViewset(viewsets.ModelViewSet):
+    queryset = Airplane.objects.all()
+    serializer_class = AirplaneSerializer
+
+    def get_serializer_class(self):
+        serializer_class = self.serializer_class
+        if self.action == "retrieve":
+            serializer_class = AirplaneDetailSerializer
+        return serializer_class
+
+    def perform_create(self, serializer):
+        serializer.save(created_by=self.request.user)
